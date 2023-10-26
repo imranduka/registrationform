f670c1e93c5a9f7d2593822718de4cc238f1397e
imranduka <iduka2003@gmail.com>
2023-10-26 14:52:11 -0400

    Updated code

--- script.js
+++ script.js
@@ -1,33 +1,31 @@
 function submitted() {
+    // Get the registration form
+    var registrationForm = document.querySelector('form');
 
-    var registrationForm = document.querySelector('form')
-    var password = document.getElementById("password").value
-    var confirmPassword = document.getElementById("confirm-password").value
-    var errorMessage = document.getElementById("passwordError")
+    // Get the values of password and confirm password fields
+    var password = document.getElementById("password").value;
+    var confirmPassword = document.getElementById("confirm-password").value;
 
+    // Get the error message element
+    var errorMessage = document.getElementById("passwordError");
+
+    // Check if the form is valid
     if (registrationForm.checkValidity() == true) {
-
+        // Check if the passwords do not match
         if (password != confirmPassword) {
-            errorMessage.style.display = 'block'
-            return false
+            errorMessage.style.display = 'block'; // Display the error message
+            return false; // Prevent form submission
         }
 
-        var successMessage = document.getElementById("success-message")
-        successMessage.style.display = 'block'
+        // Get the success message element
+        var successMessage = document.getElementById("success-message");
+        successMessage.style.display = 'block'; // Display the success message
 
-        var entireContainer = document.querySelector('.container')
-        entireContainer.style.display = 'none'
-
+        // Get the entire container and hide it
+        var entireContainer = document.querySelector('.container');
+        entireContainer.style.display = 'none';
     }
     else {
-        return false;
+        return false; // Prevent form submission if the form is not valid
     }
-
-
-
-
-
-
-
-
 }
